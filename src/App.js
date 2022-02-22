f606d117af6a4deed3f7f907e30d5e30e4df6531
Edward <fanofparadox@gmail.com>
2022-02-22 18:03:00 +0300

    blocking style added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Calculator from './Calculator';
 import './scss/desktop.scss';
 
 export default function App() {
+  const [active, setActive] = useState('item-1');
   return (
     <>
       <div id='container'>
-        <input type='radio' name='slider' id='item-1' defaultChecked/>
-        <input type='radio' name='slider' id='item-2'/>
-        <input type='radio' name='slider' id='item-3'/>
-        <input type='radio' name='slider' id='item-4'/>
-        <Calculator id={1} viewport={'width'} size={'px'}/>
-        <Calculator id={2} viewport={'width'} size={'vw'}/>
-        <Calculator id={3} viewport={'height'} size={'px'}/>
-        <Calculator id={4} viewport={'height'} size={'vh'}/>
+        <input type='radio' name='slider' id='item-1' onChange={(e) => setActive(e.target.id)} defaultChecked/>
+        <input type='radio' name='slider' id='item-2' onChange={(e) => setActive(e.target.id)}/>
+        <input type='radio' name='slider' id='item-3' onChange={(e) => setActive(e.target.id)}/>
+        <input type='radio' name='slider' id='item-4' onChange={(e) => setActive(e.target.id)}/>
+        <Calculator id={1} viewport={'width'} size={'px'} color={'#99ccff'} active={active}/>
+        <Calculator id={2} viewport={'width'} size={'vw'} color={'#80ffaa'} active={active}/>
+        <Calculator id={3} viewport={'height'} size={'px'} color={'#ffbf80'} active={active}/>
+        <Calculator id={4} viewport={'height'} size={'vh'} color={'#ff8566'} active={active}/>
       </div>
     </>
   )
--- src/Calculator.js
+++ src/Calculator.js
@@ -2,16 +2,38 @@ import React from 'react';
 
 export default function Calculator(props) {
   return (
-    <label htmlFor={'item-' + props.id} id={'card-' + props.id} className='calculator-wrap'>
+    <label 
+      htmlFor={'item-' + props.id}
+      id={'card-' + props.id} 
+      className='calculator-wrap'
+      style={{backgroundColor: props.color}}>
       <div className='viewport-wrap'>
-        <input type='text'/>
-        <label className='viewport-label'>viewport {props.viewport}</label>
+        <input 
+          className='viewport-input' 
+          type='text'
+          disabled={props.active[5] == props.id ? false : true}/>
+        <label 
+          className='viewport-label'
+          style={{background: `linear-gradient(to bottom, ${props.color} 60%, #ffffff 40%)`}}>
+            viewport {props.viewport}
+        </label>
       </div>
       <div className='size-wrap'>
-        <input type='text'/>
-        <label className='size-label'>size in {props.size}</label>
+        <input 
+          className='size-input'
+          type='text'
+          disabled={props.active[5] == props.id ? false : true}/>
+        <label 
+          className='size-label'
+          style={{background: `linear-gradient(to bottom, ${props.color} 60%, #ffffff 40%)`}}>
+            size in {props.size}
+        </label>
       </div>
-      <button>CONVERT</button>
+      <input 
+        type='button'
+        className='convert-button'
+        value='CONVERT'
+        disabled={props.active[5] == props.id ? false : true}/>
       <div className='result-wrap'>
         <span>NaN</span>
       </div>
